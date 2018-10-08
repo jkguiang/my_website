2345f50f1944924f899927decc241f287493dd77
Jonathan Guiang <jkguiang@gmail.com>
2018-10-08 11:04:42 -0700

    edited some captions/descriptions

--- src/ProjectComponents.js
+++ src/ProjectComponents.js
@@ -7,13 +7,13 @@ import { Imagetron } from './Reactrons';
 
 const projects = [
     {   "name":"AutoPlotter",
-        "desc_long":"AutoPlotter makes various plots from real CMS3 data and Monte Carlo simulations. It exports these plots as .pdf files which are then displayed on an easily accessible webpage. The example page is hosted by UCSD. All of the plot-related code is written in C++, with some Python and JavaScript responsible for the functionality of the web interface.",
-        "desc_short":"AutoPlotter is a jQuery-based interface for a program I made using C++ and ROOT that plots various information from CMS3 data.",
+        "desc_long":"AutoPlotter is a tool designed to expedite the analysis of plots made on a remote server. It has a dedicated shell script that takes a directory full of PDF (and optionally PNG and TXT) files and plots them on a public, PHP/jQuery web interface. On top of this, AutoPlotter creates a dedicated public directory for visually and internally organizing plot pages.",
+        "desc_short":"AutoPlotter is a PHP/jQuery-based interface designed to expedite plot analysis on remote servers.",
         "repo":"https://github.com/jkguiang/AutoPlotter",
         "thumb":"https://lh3.googleusercontent.com/YbzMfQYfcIVK4p-Ycqsx4ZtgFIwx7j_DAJWdmAcF_oqGdPgL9HUVWPkqZzjlLjAwt1N5j5aB7L7fBgsIh22z_qkoVj6NGfQckeKUjvahdpoeL9W4wm91LxbYgi20cvcEpq0kbMHBRRxWz-7hhOfGPe6khsojbFMQLW_CPlkY7pBOlZHrlLve0YIeJlww4SXvdRJUyHFUZBPseMqt_vQYV6jClxDkx4FHdBGaNRcHMqoF4tRz_7rQVfDyqoiQK3jdbwH5B8GHgaS3Zr_70n4G97EBqC4dzgJXCafP0MukKtLRx1uYQz-r9CmCKYLfQ4qM8BecL12H_Pu5JvcHrlM-2ivEgrLI6fywDK7Moey_Fp0tt8dwWLgIgIjTbC9IYGbGhwKb56xWDok_DcSFm76ENbRzZ_SvhZaxasisPuVasHJszLonRn_73wrYccw2MW67zFthqDycuOpka79BSJIVBhVY8lkMJ1E-4Llva0e_WkOq2aCzGFyNk-7gZH1BaSDKRqXzajJzaHtDkcNtvCbskndyRdcZ6YP0kb7WtwSJjfjqh3lPcSLexSL_xqCGOpJ9VYtyz7mtiDIVXoM8uMqk7Y1kdszumAqs1DUa9nJ9Q2khkyF--wEBQWrgZBlNNJ9eYywVJknJpn1BMXu7lppyvppyF7-bldn7qOu9r5dx5K7l2Oha8Z49i2VX=w2558-h1380-no",
         "highlights":[
             {   "name":"Web Interface: Plot Page",
-                "desc":"Most of the data analysis work I do is done on a remote server, making it a pain to sift through the many plots I often produce. To expedite this work, I wrote a simple web interface that displays plots in a more readable way.",
+                "desc":"Because viewing PDF/PNG plots can prove to be a burdensome process on a remote server, I made a simple, web-based interface for viewing them.",
                 "url":"https://github.com/jkguiang/AutoPlotter/blob/master/index.php"
             },
             {   "name":"Shell Script",
@@ -21,13 +21,13 @@ const projects = [
                 "url":"https://github.com/jkguiang/AutoPlotter/blob/master/aplot"
             },
             {   "name":"Web Interface: Repository Page",
-                "desc": "Because I make so many AutoPlotter page, it made sense to consolidate them under a single directory and beautify the navigation between them.",
+                "desc": "Because I make so many AutoPlotter pages, it made sense to consolidate them under a single directory and beautify the navigation between them.",
                 "url":"https://github.com/jkguiang/AutoPlotter/blob/master/main.php"
             }
         ]
     },
     {   "name":"AutoDQM",
-        "desc_long":"AutoDQM is a statistical tool for Data Quality Management (DQM) at the Large Hadron Particle Collider. Before AutoDQM, DQM shifters were tasked with looking at hundreds of DQM histograms, looking for hard-to-spot issues in data collection. My tool runs long established statistical tests on these graphs and outputs outliers on a simple, but effective, GUI that expedites the shifters' task.",
+        "desc_long":"AutoDQM is a statistical tool for Data Quality Management (DQM) at the Large Hadron Particle Collider. Before AutoDQM, DQM shifters were tasked with looking at hundreds of DQM histograms, looking for hard-to-spot issues in data collection. To expedite this process, AutoDQM runs long established statistical tests on these graphs and outputs outliers on a simple, but effective, GUI that expedites the shifters' task.",
         "desc_short":"AutoDQM is a statistical tool for DQM shifters. It takes the guesswork out of CERN Data Quality Management and comes packaged with a simple, but effective GUI.",
         "repo":"https://github.com/jkguiang/AutoDQM",
         "thumb":"https://lh3.googleusercontent.com/6NTm68cjsnF_WuP624t3FqFwlm9hp24MErkVjy3TuEiZ9qBbkCpV3Ez4U_Y5mBDIblD7BbFa06xPzEsMBgbibzQJma3zfhTzg-KHc1kAK8RdTQpoaeqhD-Yk1zo9R25smWdzND6QkxoGeBQVLLOPZFd5SkVkfhUxBQQPAY63pcmW33V1yceUw7IhcSwcGQB3092cmPadFO2KHT9g6YxEZnMC4eScB1AGN2WVSMachiUotGk1AgwBRoVdPajYkjAeg02402Wlzm4G-cQ_n-1DPVsa0CCJqBjbPS9LVXLHA8FibtCpRoHoyElwrDQrTHbGPlCY2pjdTqnkUXC5M_FQYgiAVoTkUCRfY5dQOMwkYuQWd7f97sJKcm0dm5adnW1z2QTz13VucbUOnE8kIM3EWaxrJLENjPUwMJfGqvg2IF0DewnUeqauaTaw0ReEU6NnJRcPpJbcGkYk1X5LKpWPA5b7aWupE58Gb5RWZ1Vxc2CogRN8nu3bbwHVjV-3mJfQDoZ5Fkwu17hZnrUiwY_R6R0A9JR1DbkQ_GYmUpn5jOhksDpzeOgOqQPX2d2g852OiWhfZ8ggX7OmFD4ob190GHv-jzfj8RAmQaUe8wNgxjKan1sfqePkwRBVOHQjPktKEf_57acpEhZy6luL4AG_sXoLtd9KUMdyoxYI5BA9qVlvyKynLFa7ZWVt=w1306-h890-no",
@@ -43,7 +43,7 @@ const projects = [
         ]
     },
     {   "name":"Photon Template Generator",
-        "desc_long":"After performing studies of the SPE response of a few Hamamatsu photomultiplier tubes, I was asked to write a program that delivered simulated responses based on the real data we took. I wrote two programs to this end, one in C++ and another in Python. Both use the real distribution of SPE response to return a random signal.",
+        "desc_long":"After performing studies of the SPE response of a few Hamamatsu photomultiplier tubes, I was asked to write a program that delivered simulated responses based on the real data we took. In collaboration with a graduate student, I wrote two programs to this end: one in C++ and another in Python. Both use the real distribution of SPE response to return a random signal.",
         "desc_short":"Generates simulated SPE responses for the Hamamatsu R878 and R7725 photomultiplier tubes for use in simulating the response of the proposed Milliqan detector.",
         "repo":"https://github.com/bjmarsh/pmt-calibration/tree/master/photon_template_generator",
         "thumb":"https://lh3.googleusercontent.com/7zNRDkRc3I-tjwkpXIaNtr83bl0d-RNagJoWbSxjwrEvO8EmUTECMXWWGEhxgKwY9_LC3TcA_Wdlq6uo2R7Df58_vQZRZLk1ois_wjS4alCuhWn8jOXYlX8_gmTXiZuLq6hePuvah_YmjIGu8xERvKaCQQsEBab35Zq1G91Wm_kvsyvU8PmA0hVocS5nFUOa0wedoMBXY1TmyK14eWzr4fuUAsbbUslpcEhpUI86o1S_XN3BPhYXRG56NHo07b7LtcIpGdkB58WFhTiuaZ6vuxURgytGaKvSZnSucrEqcILRB6joaiM9hHMCaX8hcxHGYc86OyYfKVM36QFPulXOJ2ZrTwCrLV7Or3HDZFbBNq2cAEQdeAW3q-3LT7E14W66YLOkQcssDAkZ1rMALFv0LajJ6IkMZr-r74rEr1Yjs2xZHkf-ky_etyywUmEE3dmaztTAOWasgmS5bDXKlAYcJMdI4_K5odnHlNB5DVinWKym8xFlgOJqLYHyGhTiIWmKi6eBIUFH5f_bpGyo2FVIRxzBcx4RWkqH0LSr4CHtCKcyg-UU6PZZHtQrpKsUqUDLT8laOqv2Obs6lkVagbZJ32eFe5dXUFsjFBD8u9GrSg2Ci0JC0nPNo07WM8F6649R8KHskJPDDg0hBS22DqGAHdQe3v1lQ6ARDkJMcCjF53Aisq7uGbLSzuJ-=w696-h472-no",
